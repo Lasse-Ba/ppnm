2985d36702b8c0b9aa02d03069feaf38c404b7db
Lasse <lasse.bassermann@gmx.de>
2023-02-08 09:23:27 +0100

    2-csharp

---
+++ 2-csharp/make.cs
@@ -0,0 +1,10 @@
+using System
+using static System.Console;
+using static System.Math;
+class main{
+	public static double Main(){
+	Write("hello from main\n");
+	static_hello.print();
+	static_world.print();
+	}
+}
