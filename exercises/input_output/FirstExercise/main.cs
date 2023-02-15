c3bdde850eadda250c60ac22df78145481f63e62
Lasse <lasse.bassermann@gmx.de>
2023-02-15 13:15:01 +0100

    complete exercise input/output

--- exercises/input_output/FirstExercise/main.cs
+++ exercises/input_output/FirstExercise/main.cs
@@ -3,6 +3,9 @@ using static System.Console;
 using static System.Math;
 class main{
 	public static void Main(string[] args){
+		exercise1(args);
+	}
+	static void exercise1(string[] args){
 	foreach(var arg in args){
 		var words = arg.Split(':');
 		if(words[0]=="-numbers"){
@@ -14,4 +17,18 @@ class main{
 			}
 		}
 	}
+
+	/*
+	static void exercise2(string[] args){
+	char[] split_delimiters = {' ','\t','\n'};
+	var split_options = StringSplitOptions.RemoveEmptyEntries;
+	for( string line = ReadLine(); line != null; line = ReadLine() ){
+		var numbers = line.Split(split_delimiters,split_options);
+		foreach(var number in numbers){
+			double x = double.Parse(number);
+			WriteLine($"{x} {Sin(x)} {Cos(x)}");
+			}
+		}
+	}
+*/
 }
--- exercises/input_output/SecondExercise/main.cs
+++ exercises/input_output/SecondExercise/main.cs
@@ -2,6 +2,11 @@ using System;
 using static System.Console;
 using static System.Math;
 class main{
+	public static void Main(string[] args){
+		exercise2(args);
+	}
+
+	static void exercise2(string[] args){
 	char[] split_delimiters = {' ','\t','\n'};
 	var split_options = StringSplitOptions.RemoveEmptyEntries;
 	for( string line = ReadLine(); line != null; line = ReadLine() ){
@@ -9,6 +14,8 @@ class main{
 		foreach(var number in numbers){
 			double x = double.Parse(number);
 			WriteLine($"{x} {Sin(x)} {Cos(x)}");
-                }
-        }
+			}
+		}
+	}
+
 }
