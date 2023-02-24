d26a283bd3f09ffc42816f55e7038fa625d639d0
Lasse <lasse.bassermann@gmx.de>
2023-02-24 17:03:59 +0100

    Second and Third exercise

---
+++ exercises/plots/lngamma_data.cs
@@ -0,0 +1,10 @@
+using System;
+using static System.Console;
+using static System.Math;
+class lngamma_data{
+	static void Main(){
+	for(double x= 1.0/128;x<=5;x+=1.0/64){
+		WriteLine($"{x} {sfuns.lngamma(x)}");
+	}
+}
+}
