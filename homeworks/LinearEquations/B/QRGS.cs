6692eff1b3bf8bce340e565f37888b1dfdf8fdd0
Lasse <lasse.bassermann@gmx.de>
2023-03-09 15:07:48 +0100

    Exercise linear equations, part A and B

---
+++ homeworks/LinearEquations/B/QRGS.cs
@@ -0,0 +1,55 @@
+using System;
+using static System.Console;
+using static System.Math;
+
+public class QRGS{
+    public matrix Q,R;
+
+    public QRGS(matrix a){
+        Q=a.copy();
+        int m = a.size2;
+        //WriteLine($"m={m}");
+        R = new matrix(m,m);
+        for(int i=0;i<m;i++){
+            R[i,i] = Q[i].norm();
+            Q[i]/=R[i,i];
+            for(int j=i+1;j<m;j++){
+                R[i,j]=Q[i].dot(Q[j]);
+                Q[j]-=Q[i]*R[i,j];
+            }
+        }
+    }
+
+
+
+
+    public vector solve(vector b){
+        matrix Q_t = Q.transpose();
+        vector x = Q_t*b;
+        backsub(R, x);
+        return x;
+    } 
+
+    public void backsub(matrix U, vector c){
+        for(int i=c.size-1; i>=0; i--){
+            double sum = 0;
+            for(int k=i+1;k<c.size; k++){
+                sum+= U[i,k]*c[k];
+            }
+            c[i] = (c[i]-sum)/U[i,i];
+        }
+    }
+
+    public matrix inverse(matrix A){
+        int n = A.size1;
+        matrix inverse_A = new matrix(n,n);
+        for(int i = 0; i<n; i++){
+            vector e_i = matrix.id(n)[i];
+            inverse_A[i] = solve(e_i);
+        }
+        return inverse_A;
+        }
+    }
+
+
+
---
+++ homeworks/LinearEquations/C/main.cs
@@ -0,0 +1,111 @@
+using System;
+using static System.Console;
+using static System.Math;
+using static System.Random;
+
+class main{
+    public static void Main(){
+        test_decomp();
+        test_solve();
+        test_inverse();
+    }
+
+    
+    public static void test_decomp(){
+
+        int n=4;
+        int m=3;
+
+        matrix A = new matrix(n,m);
+        Random rnd = new Random();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                A[i,j]=rnd.Next(10);
+            }
+        }
+        WriteLine("Gram-Schmidt orthonormalization");
+        WriteLine("Random matrix A");
+        A.print();
+        WriteLine();
+
+        WriteLine("Factorize A into QR:");
+		QRGS linsys = new QRGS(A);
+		//WriteLine("Q:");
+		//linsys.Q.print();
+        //linsys.Q.transpose().print();
+		WriteLine("R is (should be upper triangular):");
+		linsys.R.print();
+		matrix QtQ = linsys.Q.transpose()*linsys.Q;
+        WriteLine();
+
+        Write("Q^T*Q is (should be I):");
+        QtQ.print();
+		//WriteLine($"Q*R is approximately A: {(linsys.Q*linsys.R).approx(A)}");
+        WriteLine();
+        Write($"Q*R is (should be A)");
+        matrix QtimesR = linsys.Q*linsys.R;
+        QtimesR.print();
+		WriteLine();
+        WriteLine();
+
+    }
+    
+    public static void test_solve(){
+        int n = 4;
+
+        matrix A = new matrix(n,n);
+        vector b = new vector(n);
+        Random rnd = new Random();
+        for(int i=0; i<n; i++){
+            b[i] = rnd.Next(10);
+            for(int j=0; j<n; j++){
+                A[i,j]=rnd.Next(10);
+            }
+        }
+        WriteLine("_____________________________");
+        WriteLine("Solve the equation QRx=b");
+        WriteLine($"Matrix A: ");
+	    A.print();
+		WriteLine($"Vector b: ");
+		b.print();
+              		
+        WriteLine("Solving Ax=b.");
+		QRGS linsys = new QRGS(A);
+		vector x = linsys.solve(b);
+
+		WriteLine($"A*x is (should be equal to b):");
+        var Ax = A*x;
+        Ax.print();
+        WriteLine();
+        WriteLine();
+    }
+    
+
+    public static void test_inverse(){
+        int n = 4;
+
+        matrix A = new matrix(n,n);
+    
+        Random rnd = new Random();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                A[i,j]=rnd.Next(10);
+            }
+        }
+        WriteLine("_____________________________");
+        WriteLine("Find the inverse of A");
+        WriteLine($"Matrix A: ");
+	    A.print();
+        WriteLine();
+              		
+		QRGS linsys = new QRGS(A);
+		matrix B = linsys.inverse(A);
+        WriteLine("The inverse of matrix A is B:");
+        B.print();
+        WriteLine();
+        var AB = A*B;
+        WriteLine("The product of  A*B is (should be I):");
+        AB.print();
+    }
+    
+    }
