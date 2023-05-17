686d4936740174c2378c355b3599d05704955cf1
Lasse <lasse.bassermann@gmx.de>
2023-05-17 15:03:32 +0200

    ANN A

--- homeworks/ArtificialNeuralNetwork/A/ann.cs
+++ homeworks/ArtificialNeuralNetwork/A/ann.cs
@@ -10,14 +10,15 @@ public class ann{
 
     public ann(int n){
         this.n = n;
-        p = new vector(3 * n); // ai, bi, and wi for each hidden neuron
+        p = new vector(3 * n); /// a_i, b_i, and w_i for each hidden neuron
     }
 
-    
     public ann(vector p){
         this.n = p.size/3;
         this.p = p;
     }
+
+    
 
     public double response(double x_inp){
         double sum = 0;
@@ -37,13 +38,11 @@ public class ann{
             p[i+n] = 1.0;
             p[i+2*n] = x[0] + ( x[x.size-1] - x[0] )*i/(n-1);
         };
-        int ncalls = 0;
-        Func<vector,double> cost = (u) => {
-            ncalls ++;
-            ann ann_u = new ann(u);
+        Func<vector,double> cost = delegate(vector v){
+            ann ann_v = new ann(v);
             double sum=0;
             for (int k = 0; k < x.size; k++){
-                double diff = ann_u.response(x[k])-y[k];
+                double diff = ann_v.response(x[k])-y[k];
                 sum += diff * diff;
             };
             return sum/x.size;
@@ -53,17 +52,5 @@ public class ann{
         return p;
     }
 
-    /*
-    public double cost(vector p, vector x, vector y)
-    {
-        double cost = 0;
-        for (int k = 0; k < x.size; k++)
-        {
-            double Fp = response(x[k]);
-            double diff = Fp - y[k];
-            cost += diff * diff;
-        }
-        return cost;
-    }
-    */
+    
 }
