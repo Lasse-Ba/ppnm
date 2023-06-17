30e803c438e8eb2d0ae3807960c8dbd3b3119741
Lasse <lasse.bassermann@gmx.de>
2023-06-17 13:10:42 +0200

    It is working

--- exam/main.cs
+++ exam/main.cs
@@ -11,12 +11,29 @@ public class main{
 
 
     public static void integrate_test(){
-        Func<double,double, double> f = (x,y) => x*x*y*y;
+        Func<double,double, double> f = (x,y) => x*y;
         double a = 0.0;
         double b = 1.0;
         Func<double, double> d = x => x;
-        Func<double, double> u = x => 2.0*x;
-        double integral = quad.integ2D(f,a,b,d,u);
+        Func<double, double> u = x => x*x;
+        double acc = 1e-6;
+        double eps = 1e-6;
+        var result= quad.Integrate2D(f,a,b,d,u,acc, eps);
+        WriteLine($"{result.Item1} {result.Item2}");
+    
+
+    }
+    /*
+    public static void integrate_test_1(){
+        Func<double,double, double> f = (x,y) => x*y;
+        double a = 0.0;
+        double b = 1.0;
+        Func<double, double> d = x => x;
+        Func<double, double> u = x => x*x;
+        double acc = 1e-6;
+        double eps = 1e-6;
+        double integral = quad.Integ2D(f,a,b,d,u,acc, eps);
         WriteLine($"The integral of x^2*y^2 for x∈[0,1] and y∈[x,2x] is {integral} (should be 0.389)") ;
     }
+    */
 }
--- exam/quad.cs
+++ exam/quad.cs
@@ -4,71 +4,127 @@ using static System.Double;
 
 public static class quad{
 
-    public static double integrate1D(Func< double, double> f,
-        double a,
-        double b,
-        double acc=0.01,
-        double eps=0.01,
-        double f2=NaN,
-        double f3=NaN){
-            double h = b-a;
-
-            if(IsNaN(f2)){
-                f2 = f(a+2*h/6);
-                f3 = f(a+4*h/6);
-            } // first call, no points to reuse
-
-            double f1 = f(a+h/6.0);
-            double f4 = f(a+5.0*h/6.0);
-
-            double Q = (2*f1+f2+f3+2*f4)/6*(b-a); // higher order rule
-            double q = (  f1+f2+f3+  f4)/4*(b-a); // lower order rule
-            double err = Abs(Q-q);
-            if(err <= acc+eps*Abs(Q)) return Q;
-            else return integrate1D(f, a, (a+b)/2, acc/Sqrt(2), eps, f1, f2) +
-                        integrate1D(f, (a+b)/2, b, acc/Sqrt(2), eps, f3, f4); 
-        }
-
-    public static double integ2D(
-        Func<double, double, double> f,
-        double a,   ///lower limit x integral
-        double b,   ///upper limit x integral 
-        Func<double, double> d,  ///lower limit y integral 
-        Func<double, double> u,  ///upper limit y integral 
-        double acc=0.01,
-        double eps=0.01
-    ){
-        return IntegrateX(f,a,b,d,u);
+    public static double x_t(double t, double a, double b)
+    {
+        return a + (b - a) * t;
     }
 
-    public static double IntegrateX(
-        Func<double, double, double> f,
-        double a,   ///lower limit x integral
-        double b,   ///upper limit x integral 
-        Func<double, double> d,  ///lower limit y integral 
-        Func<double, double> u,  ///upper limit y integral
-        double acc=0.01,
-        double eps=0.01
-    ){
-        Func<double, double> innerIntegrand = y => f(a,y)*u(y);
-        Func<double, double> integrand = x =>
+    public static double w(double w, double a, double b)
+    {
+        return (b - a) * w;
+    }
+
+    public static double d(double x)
+    {
+        return x;
+    }
+
+    public static double u(double x)
+    {
+        return x*x;
+    }
+
+
+    public static double Driver1D(
+        Func<double, double, double> f, 
+        double x, 
+        double a, 
+        double b, 
+        double acc, 
+        double eps,
+        double two, 
+        double three, 
+        double[] high, 
+        double[] low, 
+        double[] xs)
+    {
+        double one = f(x, x_t(xs[0], a, b));
+        double four = f(x, x_t(xs[3], a, b));
+        double Q = w(high[0], a, b) * one + w(high[1], a, b) * two + w(high[2], a, b) * three + w(high[3], a, b) * four;
+        double q = w(low[0], a, b) * one + w(low[1], a, b) * two + w(low[2], a, b) * three + w(low[3], a, b) * four;
+        double err = Math.Abs(Q - q);
+        if (err < acc + eps * Math.Abs(Q))
         {
-            double innerA = d(x);
-            double innerB = u(x);
-            return IntegrateY(innerIntegrand, innerA, innerB);
-        };
-        return integrate1D(integrand, a, b);
+            return Q;
+        }
+        else
+        {
+            return Driver1D(f, x, a, (a + b) / 2, acc / Math.Sqrt(2.0), eps, one, two, high, low, xs) +
+                   Driver1D(f, x, (a + b) / 2, b, acc / Math.Sqrt(2.0), eps, three, four, high, low, xs);
+        }
     }
 
-    public static double IntegrateY(
-        Func< double, double> f,
-        double a,
-        double b,
-        double acc=0.01,
-        double eps=0.01
-    ){
-        Func<double, double> integrand = y => f(y); // Ich glaube, hier ist der Fehler
-        return integrate1D(integrand, a, b);
+    // RECURSIVE INTEGRATION FUNCTION 2D
+    public static double Driver2D(
+        Func<double, double, double> f,
+        double a, 
+        double b, 
+        double acc, 
+        double eps,
+        double two,
+        double three,
+        double[] high,
+        double[] low,
+        double[] xs,
+        ref double error)
+    {
+        double x1 = x_t(xs[0], a, b);
+        double a1 = d(x1);
+        double b1 = u(x1);
+
+        double x4 = x_t(xs[3], a, b);
+        double a4 = d(x4);
+        double b4 = u(x4);
+
+        double one = Driver1D(f, x1, a1, b1, acc, eps, f(x1, x_t(xs[1], a1, b1)), f(x1, x_t(xs[2], a1, b1)), high, low, xs);
+        double four = Driver1D(f, x4, a4, b4, acc, eps, f(x4, x_t(xs[1], a4, b4)), f(x4, x_t(xs[2], a4, b4)), high, low, xs);
+        double Q = w(high[0], a, b) * one + w(high[1], a, b) * two + w(high[2], a, b) * three + w(high[3], a, b) * four;
+        double q = w(low[0], a, b) * one + w(low[1], a, b) * two + w(low[2], a, b) * three + w(low[3], a, b) * four;
+        double err = Math.Abs(Q - q);
+
+        if (err < acc + eps * Math.Abs(Q))
+        {
+            error += err;
+            return Q;
+        }
+        else
+        {
+            return Driver2D(f, a, (a + b) / 2, acc / Math.Sqrt(2.0), eps, one, two, high, low, xs, ref error) +
+                   Driver2D(f, (a + b) / 2, b, acc / Math.Sqrt(2.0), eps, three, four, high, low, xs, ref error);
+        }
     }
 
-}
+    public static Tuple<double, double> Integrate2D(
+        Func<double, double, double> f,
+        double a, 
+        double b, 
+        Func<double, double> d,
+        Func<double, double> u,
+        double acc,
+        double eps)
+    {
+        double[] Higher_order = { 2.0 / 6, 1.0 / 6, 1.0 / 6, 2.0 / 6 };
+        double[] Lower_order = { 1.0 / 4, 1.0 / 4, 1.0 / 4, 1.0 / 4 };
+        double[] interval_points = { 1.0 / 6, 2.0 / 6, 4.0 / 6, 5.0 / 6 };
+
+        double estimate = 0;
+        double error = 0;
+
+        double x2 = x_t(interval_points[1], a, b);
+        double a2 = d(x2);
+        double b2 = u(x2);
+
+        double x3 = x_t(interval_points[2], a, b);
+        double a3 = d(x3);
+        double b3 = u(x3);
+
+        double start2 = Driver1D(f, x2, a2, b2, acc, eps, f(x2, x_t(interval_points[1], a2, b2)), f(x2, x_t(interval_points[2], a2, b2)),Higher_order,Lower_order,interval_points);
+        double start3 = Driver1D(f, x3, a3, b3, acc, eps, f(x3, x_t(interval_points[1], a3, b3)), f(x3, x_t(interval_points[2], a3, b3)),Higher_order,Lower_order,interval_points);
+
+        estimate = Driver2D(f, a, b, acc, eps, start2, start3, Higher_order, Lower_order, interval_points, ref error);
+
+        return Tuple.Create(estimate, Math.Sqrt(error));
+    }
+
+
+    }
